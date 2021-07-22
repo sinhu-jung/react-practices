e00e21dabddd081b6337a545f3e6f9cbc88d79aa
sinhu-jung <tlsgn26@naver.com>
2021-07-22 12:51:15 +0900

    ex06

---
+++ basics/webpack-practices/ex06/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+const App = () => {
+    const app = document.createElement('h1');
+    app.className = 'App';
+    app.innerHTML = 
+    '<header class="App-header">' + 
+
+    '</header>';
+    return app;
+}
+export { App };
---
+++ basics/webpack-practices/ex06/src/index.js
@@ -0,0 +1,3 @@
+import { App } from './App';
+import './index.css';
+document.getElementById('root').appendChild(App());
