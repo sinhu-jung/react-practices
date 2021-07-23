1053ec14d046ba3d8021aa7c8d4662e4c17d72fc
sinhu-jung <tlsgn26@naver.com>
2021-07-23 13:41:11 +0900

    react baisc ex07 cra 만들어보기

---
+++ basics/ex07/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve('src/index.js'),
+    output: {
+        path: path.resolve('public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js/i,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }, {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.svg$/i,
+            loader: 'file-loader',
+            options: {
+                outputPath: '/assets/images',
+                name: '[name].[ext]'
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve('public'),
+        host: "0.0.0.0",
+        port: 9999,
+        inline: true,
+        liveReload: true,
+        hot: false,
+        compress: true,
+        historyApiFallback: true
+    }
+} 
