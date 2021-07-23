1f298954430e47f117249965a451e39cc943a9fd
sinhu-jung <tlsgn26@naver.com>
2021-07-23 09:56:07 +0900

    react babel

---
+++ basics/ex06/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    const message = 'Hello World';
+    return (
+        <div>
+            <h1>{message}</h1>
+        </div>
+    )
+}
+
+export default App;
