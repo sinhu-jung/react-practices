2c4e38017606cb9694290426d99273162f9e78b8
sinhu-jung <tlsgn26@naver.com>
2021-07-26 11:06:48 +0900

    ex08

--- 01.basics/ex08/README.md
+++ 01.basics/ex08/README.md
@@ -1,8 +1,16 @@
 ## ex08: JSX Tutorials
 
+[react 코드 변환](https://babeljs.io/repl)
+
 ### 01. 특징 01: HTML과의 차이점
 ### 02. 특징 02: Single Root Node
-
+### 03. JSX로 함수 컴포넌트 작성
+### 04. Pure React(Rect API)로 함수 컴포넌트 작성
+### 05. 클래스 컴포넌트 작성하기
+### 06. 특징 03: JSX 표현식 표기법 { _expression_ } 문제점: if-statment
+### 07. 특징 04: 공백(Blank Space)
+### 08. Dynamic HTML Rendering
+### 09. Comment
 
 ### Run Examples
 ```bash
---
+++ 01.basics/ex08/src/01/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function() {
+    return (
+        <div>
+            <h2>App01</h2>
+            <p>JSX Tutorials - 특징 01: HTML과의 차이점</p>
+            {/* 
+                1. 속성은 Camel Case
+            */}
+            <input type='text' maxLength='10'/>
+            {/* 
+                2. Element는 꼭 닫혀야 한다.
+                (오류) <br>, <hr>, <input type='text'>, <img src='>
+            */}
+            <br/>
+            <hr/>
+            <img width='100px' src='https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png' />
+            {/* 
+               3. 속성 이름은 DOM API 기반이다.
+               <div id='box' class='box'></div>
+               document.getElementById('box').className = 'box'
+            */}
+            <div id='box' className='box'>
+                box 입니다.
+            </div>
+
+        </div>
+    );
+}
