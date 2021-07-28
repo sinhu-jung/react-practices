959dbaa801429e4bdb4bca0128f08b7661e35e2c
sinhu-jung <tlsgn26@naver.com>
2021-07-28 09:53:03 +0900

    kanban #1

---
+++ 02.component/kanban/src/ex01/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class extends Component {
+    render() {
+        return (
+            <div id="App">
+                <h1>Kanban Board</h1>
+            </div>
+        );
+    }
+}
