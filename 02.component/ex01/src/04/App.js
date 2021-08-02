26fe5b412e3acb237516ea60a98c86b7b0dd4f5b
sinhu-jung <tlsgn26@naver.com>
2021-08-02 14:49:00 +0900

    propTypes Kanban

--- 02.component/ex01/src/04/App.js
+++ 02.component/ex01/src/04/App.js
@@ -5,7 +5,17 @@ export default function App() {
 
     return (
         <div id="App">
-            <MyComponent props01={ 10 }/>
+            <MyComponent 
+                /*props01={ '문자열' } : Not Required (Default Value)*/
+                props02 = { 1 }
+                props03 = { true }
+                props04 = { { no:1 } }
+                props05 = {[1, 2, 3, 4]}
+                props06 = { () => '함수' }
+                props07 = { 100 }
+                props08 = { [true, false, true, false] }
+                props09 = { {no:1, name:'둘리', email:'dooly@gmail.com'}}
+                />
         </div>
     );
 }
