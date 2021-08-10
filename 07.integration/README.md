246443fb40c9d4630c0041cc77a4690bd8247d71
sinhu-jung <tlsgn26@naver.com>
2021-08-10 09:45:40 +0900

    app02 kanban

--- 07.integration/README.md
+++ 07.integration/README.md
@@ -47,6 +47,11 @@
     - AJAX: fetch 기반
 
 2. App02:Kanban(node + react) = React(Frontend) Dev Integration With Node(Backend)
+    - 개발 통합 설정 (With Node Backend)
+    - backend: Fully API (Node Express기반)
+    - Frontend: React (SPA)
+    - AJAX: fetch 기반
+    
 3. App03:Guestbook(node + react) = React(Frontend) Dev Integration With Node(Backend)
 4. App04:Gallery(node + react) = React(Frontend) Dev Integration With Node(Backend)
 5. App05:mysite(App03, App04) = React(Frontend) Dev Integration With Node(Backend)
--- 07.integration/app01.emaillist/backend/models/emaillist.js
+++ 07.integration/app01.emaillist/backend/models/emaillist.js
@@ -9,7 +9,7 @@ module.exports = {
         //const query = (sql, data) => new Promise((resolve, reject) => conn.query(sql, data, (error, rows, field) => error ? reject(error) : resolve(rows)));
         const query = util.promisify(conn.query).bind(conn);
         try {
-            return results = await query("select first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
+            return results = await query("select no, first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
         } catch(e) {
             console.error(e);
         } finally {
