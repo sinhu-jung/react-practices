5db27757800c7dc8bc0176e1e43486779c869df2
sinhu-jung <tlsgn26@naver.com>
2021-08-17 11:51:50 +0900

    routing#2 #3

---
+++ 03.routing/src/ex03/component/Guestbook.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Guestbook() {
+    return (
+        <Fragment>
+            <div id='header'>
+                <h1><NavLink to={'/'} >Header</NavLink></h1>
+            </div>
+
+            <div id='contents'>
+                <h2>Guestbook</h2>
+            </div>
+
+            <div id='navigation' >
+                <ul>
+                    <li><NavLink to={'/guestbook'} >[Guestbook]</NavLink></li>
+                    <li><NavLink to={'/gallery'} >[Gallery]</NavLink></li>
+                </ul>
+            </div>
+
+            <div id='footer'>
+                <p>(c)opyright 2021</p>
+            </div>
+
+        </Fragment>
+    );
+}
