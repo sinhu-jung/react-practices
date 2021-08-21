eadf64c9c395cedb77a63c91354cc825f8545e42
sinhu-jung <tlsgn26@naver.com>
2021-08-22 07:50:09 +0900

    react-practices 07.integration app05.mysite

---
+++ 07.integration/app05.mysite/backend/routes/index.js
@@ -0,0 +1,19 @@
+const errorRouter = require('./error');
+const authorized = require('./authorized');
+
+const applicationRouter = {
+    setup: async function (application) {
+        application
+            .all('*', function (req, res, next) {
+                res.locals.req = req;
+                res.locals.res = res;
+                next();
+            })
+            .use('/api/gallery', require('./gallery'))
+            .use('/api/guestbook', require('./guestbook'))
+            .use(errorRouter.error404)
+            .use(errorRouter.error500)
+    }
+};
+
+module.exports = {applicationRouter};
